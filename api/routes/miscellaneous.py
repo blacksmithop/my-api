d293207b0d2f8587a3656e10cbce0ef709969cbe
blacksmithop <angstycoder101@gmail.com>
2022-05-08 17:58:00 +0530

    Added template for testing endpoint

--- api/routes/miscellaneous.py
+++ api/routes/miscellaneous.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 from time import localtime, strftime
 
 misc = Blueprint('misc', __name__, template_folder='templates')
@@ -9,3 +9,9 @@ def currentTime():
     """Return the current time"""
     return jsonify({'time': strftime('%H:%M:%S', localtime()),
                     'message': 'Current time'}), 200
+
+
+@misc.route('/testapi', methods=['GET'])
+def testAPI():
+    """Test an API endpoint"""
+    return render_template('testapi.html')
